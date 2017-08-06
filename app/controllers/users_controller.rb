0d969b0bc50ccbfb6569213f25d1868cce88e1ae
Babasanya <babscraig@gmail.com>
2017-08-07 00:15:25 +0100

    Add signup form with validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @time_now = Time.now
+  end
+
+  def test
+    @text = "Controller: #{params[:controller].to_s}"
+    @more_text = "Action: #{params[:action].to_s}"
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
